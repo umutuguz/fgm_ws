4829b007e941f147111e79407517c76412ec8c3b
umutuguz <umutuguzlar7@gmail.com>
2022-12-11 14:20:06 +0300

    Yayın frekansı parametrik olarak ayarlanabilir hale getirildi.

--- people_tracker/src/listener_tracked_person.py
+++ people_tracker/src/listener_tracked_person.py
@@ -7,15 +7,16 @@ from geometry_msgs.msg import PoseStamped, Pose
 
 def newMessageReceived(TrackedPersons):
     poseStamped = PoseStamped()
+    rate = rospy.Rate(1)
 
     for TrackedPerson in TrackedPersons.tracks:
 
         poseStamped.header = TrackedPersons.header
         poseStamped.pose.position = TrackedPerson.pose.pose.position
         poseStamped.pose.orientation = TrackedPerson.pose.pose.orientation
-
-    pub.publish(poseStamped)
     
+    pub.publish(poseStamped)    
+    rate.sleep()
     
 # Initialize node
 rospy.init_node("tracked_persons_to_move_base_goal")
@@ -29,10 +30,3 @@ sub = rospy.Subscriber(inputTopic, TrackedPersons,
 pub = rospy.Publisher(outputTopic, PoseStamped, queue_size=20)
 
 rospy.spin()
-
-# rate = rospy.Rate(1)
-
-# while not rospy.is_shutdown():
-#     pub = rospy.Publisher(outputTopic, PoseStamped, queue_size=20)
-#     rate.sleep()
-#     rospy.spin()
