edab48e1dcc9b94b8c6df0f3916584ec54857812
umutuguz <umutuguzlar7@gmail.com>
2022-12-14 01:32:06 +0300

    Laser Tracker Filter eklendi. Float64 ile - hatası çözülmeli.

--- people_tracker/src/laser_tracker_filter.py
+++ people_tracker/src/laser_tracker_filter.py
@@ -5,11 +5,84 @@ import math
 import tf
 from spencer_tracking_msgs.msg import TrackedPersons, TrackedPerson
 from geometry_msgs.msg import PoseStamped,PoseWithCovarianceStamped, PoseWithCovariance, Pose
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64, Bool
 from sensor_msgs.msg import LaserScan
 
 class laser_tracker_filter():
     
     def __init__(self):
         rospy.init_node("laser_tracker_filter")
+        self.laser_publisher = rospy.Publisher('/scan_track_filtered', LaserScan, queue_size = 10)
+        # self.laser_subscriber = rospy.Subscriber('/scan', LaserScan, self.laserCallback)
+        self.laser_subscriber = rospy.Subscriber('/front_rp/rp_scan_filtered_front', LaserScan, self.laserCallback)
+        self.theta_subscriber = rospy.Subscriber('/track_theta', Float64, self.thetaCallback)
+        self.theta_subscriber = rospy.Subscriber('/dist_to_goal', Float64, self.distCallback)
+        self.tracking_status_subscriber = rospy.Subscriber('/tracking_status', Bool, self.trackingCallback)
+        self.laserScan = LaserScan()
+        self.trackTheta = 0.0
+        self.track_index = 0
+        self.dist_to_goal = 0.0
+        self.filter_indexs = 0
+        self.counter = 0
+        self.index_pos = 0
+        self.index_neg = 0
+        self.tracking_status = False
+    
+    def publish_laser(self):
         
+        self.laser_publisher.publish(self.laserScan)
+        
+    def thetaCallback(self, msg):
+        
+        self.trackTheta = msg
+
+    def laserCallback(self, msg):
+        
+        self.laserScan = msg
+        
+    def distCallback(self, msg):
+        
+        self.dist_to_goal = msg
+    
+    def trackingCallback(self, msg):
+        
+        self.tracking_status = msg
+        
+    def compute_range_index(self):
+        
+        self.track_index = round((self.trackTheta - 8.5) * (344.0 / 163.0))
+
+        
+        if self.track_index <= 344:
+            self.track_index = 343
+
+    def filter_laser_ranges(self):
+        
+        if self.tracking_status == True:
+            
+            self.filter_indexs = round((-2.5 * self.dist_to_goal) + 17)
+            
+            for self.counter in range(1, (self.filter_indexs + 1)):
+                self.index_pos = self.track_index + self.counter
+                self.index_neg = self.track_index - self.counter
+                # self.laserScan.ranges[self.index_pos] = math.inf
+                self.laserScan.ranges[self.index_pos] = 99
+                # self.laserScan.ranges[self.index_neg] = math.inf
+                self.laserScan.ranges[self.index_neg] = 99
+                # self.laserScan.ranges[self.track_index] = math.inf
+                self.laserScan.ranges[self.track_index] = 99
+                rospy.loginfo(self.laserScan.ranges[self.track_index])
+            
+        
+
+if __name__ == '__main__':
+    
+    laser_tracker_filter_obj = laser_tracker_filter()
+    rospy.loginfo("Filtered Laser data published!")
+    while not rospy.is_shutdown():
+        laser_tracker_filter_obj.compute_range_index()
+        laser_tracker_filter_obj.filter_laser_ranges()
+        laser_tracker_filter_obj.publish_laser()
+            
+        
+rospy.spin()
--- people_tracker/src/people_tracker.py
+++ people_tracker/src/people_tracker.py
@@ -5,7 +5,7 @@ import math
 import tf
 from spencer_tracking_msgs.msg import TrackedPersons, TrackedPerson
 from geometry_msgs.msg import PoseStamped,PoseWithCovarianceStamped, PoseWithCovariance, Pose
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64, Bool
 from tf.transformations import euler_from_quaternion
 class people_tracker():
     
@@ -14,14 +14,16 @@ class people_tracker():
         self.dist_publisher = rospy.Publisher('/dist_to_goal', Float64, queue_size = 10)
         self.ref_publisher = rospy.Publisher('/setpoint', Float64, queue_size = 10)
         self.theta_publisher = rospy.Publisher('/track_theta', Float64, queue_size = 10)
+        self.tracking_publisher = rospy.Publisher('/tracking_status', Bool, queue_size = 10)
         self.goal_publisher = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size = 10)
         self.pose_subscriber = rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, self.pose_callback)
         self.tracker_subscriber = rospy.Subscriber('/spencer/perception/tracked_persons', TrackedPersons, self.tracker_callback)
         self.poseStamped = PoseStamped()
         self.trackedPersons = TrackedPersons()
         self.poseWithCovarienceStamped = PoseWithCovarianceStamped()
-        self.tf_listener = tf.TransformListener()
+        # self.tf_listener = tf.TransformListener()
         self.distToGoal = 0.0
+        self.tracking_status = False
         self.diffX = 0.0
         self.diffY = 0.0
         self.set_point = -1.0
@@ -60,6 +62,12 @@ class people_tracker():
             # rospy.loginfo("Setpoint published!")
             # self.rate_10.sleep()
     
+    def publish_tracking_status(self):
+        
+            self.tracking_publisher.publish(self.tracking_status)
+            # rospy.loginfo("Setpoint published!")
+            # self.rate_10.sleep()
+    
     def shutdownhook(self):
         self.ctrl_c = True
         
@@ -68,8 +76,12 @@ class people_tracker():
     
     def tracker_callback(self,msg):
 
+        self.poseStamped.header = msg.header
         for TrackedPerson in msg.tracks:
-            self.poseStamped.header = msg.header
+            if TrackedPerson.track_id == '':
+                self.tracking_status = False
+            else:
+                self.tracking_status = True
             self.poseStamped.pose.position = TrackedPerson.pose.pose.position
             self.poseStamped.pose.orientation = TrackedPerson.pose.pose.orientation
     
@@ -79,8 +91,10 @@ class people_tracker():
         self.diffX = self.poseStamped.pose.position.x - self.poseWithCovarienceStamped.pose.pose.position.x 
         # self.diffY = self.poseWithCovarienceStamped.pose.pose.position.y - self.poseStamped.pose.position.y
         self.diffY = self.poseStamped.pose.position.y - self.poseWithCovarienceStamped.pose.pose.position.y 
-        
-        self.distToGoal = -(math.hypot(self.diffX, self.diffY))
+        if self.tracking_status == True:
+            self.distToGoal = -(math.hypot(self.diffX, self.diffY))
+        else:
+            self.distToGoal = 0.0
     
     def compute_goal(self):
         a = 0
@@ -139,17 +153,22 @@ if __name__ == '__main__':
         # people_tracker_obj.compute_goal()
         people_tracker_obj.publish_dist()
         # rospy.loginfo("Dist to goal published!" )
-        # people_tracker_obj.publish_goal()
-        # rospy.loginfo("Goal published!")
+        if people_tracker_obj.tracking_status == True:
+            # people_tracker_obj.publish_goal()
+            rospy.loginfo("Goal published!")
+        else:
+            rospy.loginfo("There is no person to track!")
         people_tracker_obj.publish_ref()
         # rospy.loginfo("Setpoint published!")
         people_tracker_obj.compute_theta()
         # rospy.loginfo("Computing theta!")
         people_tracker_obj.publish_theta()
         # rospy.loginfo("Theta published!")
-        rospy.loginfo("theta is: %f" %people_tracker_obj.theta)
-        rospy.loginfo("pose_theta is: %f" %people_tracker_obj.pose_theta)
-        rospy.loginfo("theta_laser is: %f" %people_tracker_obj.theta_laser)
+        people_tracker_obj.publish_tracking_status()
+        # rospy.loginfo("theta is: %f" %people_tracker_obj.theta)
+        # rospy.loginfo("pose_theta is: %f" %people_tracker_obj.pose_theta)
+        # rospy.loginfo("theta_laser is: %f" %people_tracker_obj.theta_laser)
+        rospy.loginfo(people_tracker_obj.tracking_status)
         people_tracker_obj.rate_1.sleep()
         
     
